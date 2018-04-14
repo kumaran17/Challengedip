b8ff1638c72f354605ffd90f4cddc98ca1ddbf7b
kumaran <kmrntsk@gmail.com>
2018-04-14 15:14:39 +1200

    test case for homepage has been added

--- testcase.py
+++ testcase.py
@@ -1,7 +1,10 @@
 import unittest #for writing test case
 import os #for mkdir and path
 from selenium import webdriver #for selenium to run
-from selenium.webdriver.common.keys import Keys #for common functions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By #explicit wait
+from selenium.webdriver.support.ui import WebDriverWait #explicit wait
+from selenium.webdriver.support import expected_conditions as EC #explicit wait
 from datetime import datetime #for date and time
 now = datetime.now()
 print(now.strftime("Today's Date is %A %d %B %Y"))
@@ -13,10 +16,10 @@ class Login(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
         
-    def test_a1(self): #wrong password
+    def test_a1(self): #wrong password for login
         driver = self.driver
-        driver.maximize_window()
         self.driver.get('http://www.challengedip.com')
         element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
         element = driver.find_element_by_id("emailaddress")
@@ -29,9 +32,8 @@ class Login(unittest.TestCase):
         print(Login.save_dir)
         print('test case wrong password')
 
-    def test_a2(self): #wrong email
+    def test_a2(self): #wrong email for login
         driver = self.driver
-        driver.maximize_window()
         self.driver.get('http://www.challengedip.com')
         element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
         element = driver.find_element_by_id("emailaddress")
@@ -42,9 +44,8 @@ class Login(unittest.TestCase):
         self.driver.save_screenshot(os.path.join(Login.save_dir, 'wrong email address.png'))
         print('test case wrong email')
 
-    def test_a3(self): #wrong email and password
+    def test_a3(self): #wrong email and password for login
         driver = self.driver
-        driver.maximize_window()
         self.driver.get('http://www.challengedip.com')
         element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
         element = driver.find_element_by_id("emailaddress")
@@ -55,16 +56,15 @@ class Login(unittest.TestCase):
         self.driver.save_screenshot(os.path.join(Login.save_dir, 'wrong emailpassword.png'))        
         print('test case wrong email and password')
 
-    def test_a4(self): #correct credentials
+    def test_a4(self): #correct credentials for login
         driver = self.driver
-        driver.maximize_window()
         self.driver.get('http://www.challengedip.com')
         element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
         element = driver.find_element_by_id("emailaddress")
         element.send_keys("kumarants007@gmail.com")
         element = driver.find_element_by_id("password")
         element.send_keys("password")
-        element = driver.find_element_by_id("login-submit").click()
+        element = driver.find_element_by_id("login-submit").click()      
         self.driver.save_screenshot(os.path.join(Login.save_dir, 'correct credentials.png'))
         print('test case correct credentials')
 
@@ -74,10 +74,10 @@ class Logout(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
                 
-    def test_b1(self): #to check the logout functionality
+    def test_b1(self): #to check logout
         driver = self.driver
-        driver.maximize_window()
         self.driver.get('http://www.challengedip.com')
         element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
         element = driver.find_element_by_id("emailaddress")
@@ -89,10 +89,8 @@ class Logout(unittest.TestCase):
         element = driver.find_element_by_link_text('LOGOUT').click()
         self.driver.save_screenshot(os.path.join(Logout.save_dir, 'logout.png'))
         print('screenshot location is')
-        print(Login.save_dir)
+        print(Logout.save_dir)
         print('test check logout')
-    def tearDown(self):
-        self.driver.close()
 
 class Contact(unittest.TestCase):
     save_dir = 'C:\\Users\\dhamilton\\Desktop\\Selenium project\\output\\contact\\'+datetime.now() .strftime("%d-%m-%Y %I-%M %p")
@@ -100,10 +98,10 @@ class Contact(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-        
-    def test_c1(self): #to check the logout functionality
+        self.driver.maximize_window()
+
+    def test_c1(self): #to check contact page
         driver = self.driver
-        driver.maximize_window()
         self.driver.get('http://www.challengedip.com')
         element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
         element = driver.find_element_by_id("emailaddress")
@@ -122,8 +120,45 @@ class Contact(unittest.TestCase):
         element = driver.find_element_by_id('contact-submit').click()
         self.driver.save_screenshot(os.path.join(Contact.save_dir, 'submitted form.png'))
         print('screenshot location is')
-        print(Login.save_dir)
+        print(Contact.save_dir)
         print('test case contact')
+
+class HomePage(unittest.TestCase):
+    save_dir = 'C:\\Users\\dhamilton\\Desktop\\Selenium project\\output\\homepage\\'+datetime.now() .strftime("%d-%m-%Y %I-%M %p")
+    os.mkdir(save_dir)
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        
+    def test_d1(self): #to check homepage
+        driver = self.driver
+        self.driver.get('http://www.challengedip.com')
+        element = driver.find_element_by_xpath("//*[@id='navbarResponsive']/ul/li[1]/a").click()
+        element = driver.find_element_by_id("emailaddress")
+        element.send_keys("kumarants007@gmail.com")
+        element = driver.find_element_by_id("password")
+        element.send_keys("password")
+        element = driver.find_element_by_id("login-submit").click()
+        self.driver.save_screenshot(os.path.join(HomePage.save_dir, 'login.png'))
+        self.driver.find_element_by_link_text('CONTACT').click()
+        self.driver.save_screenshot(os.path.join(HomePage.save_dir, 'contact.png'))
+        element = driver.find_element_by_link_text("CHALLENGE DIP").click()
+        self.driver.save_screenshot(os.path.join(HomePage.save_dir, "homepage after login.png"))
+        element = driver.find_element_by_link_text("Start the Test").click()
+        driver.implicitly_wait(1)
+        element = driver.find_element_by_link_text("Youtube Voice Challenge").click()
+        element = driver.find_element_by_id("showbutton").click()
+        driver.implicitly_wait(3)
+        element = driver.find_element_by_link_text("CHALLENGE DIP").click()
+        element = driver.find_element_by_tag_name('h1')
+        self.driver.save_screenshot(os.path.join(HomePage.save_dir, "homepage after challenge.png"))
+        print('screenshot location is')
+        print(HomePage.save_dir)
+        print('test case homepage')
+
+def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
